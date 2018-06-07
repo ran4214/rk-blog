650c69236f7e2d29490e7a27651a0e80bf62664e
배진영 <ran4214@naver.com>
2018-06-07 17:52:35 +0900

    인텔리제이 메이븐 프로젝트 재구성 및 헤더 수정중

---
+++ src/main/java/com/rkblog/RkBlogApplication.java
@@ -0,0 +1,12 @@
+package com.rkblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkBlogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rkblog/RkBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rkblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RkBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
