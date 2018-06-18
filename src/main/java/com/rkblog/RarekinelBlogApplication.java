0481d29c7dbe22727e257147561dfb4b4bb5f8b6
배진영 <ran4214@naver.com>
2018-06-18 21:18:55 +0900

    replace new Project

--- src/main/java/com/rkblog/RarekinelBlogApplication.java
+++ src/main/java/com/rkblog/RarekinelBlogApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RkBlogApplication {
+public class RarekinelBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RkBlogApplication.class, args);
+        SpringApplication.run(RarekinelBlogApplication.class, args);
     }
 }
